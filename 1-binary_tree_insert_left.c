94094f7312a0fe0a770c99a8d9936177fc8aa873
saif-sabit <saifsabit@gmail.com>
2023-09-01 00:53:52 +0300

    Betty Style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,32 +9,26 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    // Check if the parent is NULL
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    // Create a new node with the given value and the provided parent
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-    {
-        return (NULL); // Return NULL if memory allocation fails
-    }
+	/* If the parent already has a left child */
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
 
-    // If the parent already has a left child
-    if (parent->left != NULL)
-    {
-        // Move the existing left child down one level
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-    }
+	parent->left = new_node;
 
-    // Set the new node as the left child of the parent
-    parent->left = new_node;
-
-    return (new_node); // Return the new node
+	return (new_node);
 }
-
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Inserts a node as the right child of a parent node.
+ * binary_tree_insert_right - Inserts
+ * a node as the right child of a parent node.
  * If the parent already has a right child, the new node takes its place.
  * @parent: Pointer to the parent node.
  * @value: Value to be assigned to the new node.
@@ -9,32 +10,28 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    // Check if the parent is NULL
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-    // Create a new node with the given value and the provided parent
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-    {
-        return (NULL); // Return NULL if memory allocation fails
-    }
+	/* Create a new node with the given value and the provided parent */
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    // If the parent already has a right child
-    if (parent->right != NULL)
-    {
-        // Move the existing right child down one level
-        new_node->right = parent->right;
-        parent->right->parent = new_node;
-    }
+	/* If the parent already has a right child */
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
 
-    // Set the new node as the right child of the parent
-    parent->right = new_node;
+	parent->right = new_node;
 
-    return (new_node); // Return the new node
+	return (new_node);
 }
-
